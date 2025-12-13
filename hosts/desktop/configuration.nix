990a8e1e23bd2cfb702d74531ffa02043836d5bf
Henrik Nepstad <github@topheinz.com>
2025-12-13 16:20:58 +0100

    Moved bootloader from hardwareconfig.nix to configuration.nix for desktop

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -41,6 +41,9 @@
     ];
   };
 
+  # Bootloader
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
--- hosts/desktop/hardware-configuration.nix
+++ hosts/desktop/hardware-configuration.nix
@@ -30,8 +30,4 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-
-  # Bootloader
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
 }
