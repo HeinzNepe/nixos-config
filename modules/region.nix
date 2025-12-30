163f4029b41889fac1b76d81bf2c2fa3b30ae3a6
Henrik Nepstad <github@topheinz.com>
2025-12-30 20:52:13 +0100

    Update the region file, so the hardware clock is stored in local time (to make dualbooting better)

--- modules/region.nix
+++ modules/region.nix
@@ -4,6 +4,9 @@
   # Set time zone
   time.timeZone = "Europe/Oslo";
 
+  # Set the hardware clock to local time
+  time.hardwareClockInLocalTime = true;
+
   # Select internationalisation properties. 
   i18n.defaultLocale = "en_GB.UTF-8";
 
