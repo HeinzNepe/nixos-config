c02d8075a84c0e826a1bf096a6e57736855c96e6
Henrik Nepstad <github@topheinz.com>
2026-01-12 13:28:19 +0100

    Rename variable in vars (me slightly dum dum)

--- vars.nix
+++ vars.nix
@@ -5,7 +5,7 @@
   userEmail = "mail@topheinz.com";
 
   # Default password for new machines (generate with: mkpasswd -m sha-512)
-  standardPasswordHash = "$6$tsKkFmzTKdHpfz/S$84YR0GJWGYVk3rGeKPSdeX.YaFyZV8V7PUWQLzrZvoo0CAt2WwuxyjQwCLOBx.BRecJsoAxKU9w6wF04Z57FY."; 
+  hashedPassword = "$6$tsKkFmzTKdHpfz/S$84YR0GJWGYVk3rGeKPSdeX.YaFyZV8V7PUWQLzrZvoo0CAt2WwuxyjQwCLOBx.BRecJsoAxKU9w6wF04Z57FY."; 
   
   # SSH public keys
   sshPublicKeyPersonal = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHlcSbSiViVlhO3v2jz7U0NYBi8hags7R0TCvhIFSlgA"; # Portunus-Alfa
