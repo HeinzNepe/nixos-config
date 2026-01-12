c3e47988f4f861d994f84ecde7a6dd0c80b1b3b6
Henrik Nepstad <github@topheinz.com>
2026-01-12 10:53:08 +0100

    Switch bootloader configuration to GRUB in configuration.nix

--- hosts/core-vm-gitea-01/configuration.nix
+++ hosts/core-vm-gitea-01/configuration.nix
@@ -68,9 +68,10 @@
     ];
   };
 
-  # Bootloader
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  # Bootloader (grub since its non-uefi)
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.useOSProber = false;
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
