40b55f3a907f78419ed711f082f7277744338625
Henrik Nepstad <github@topheinz.com>
2026-02-28 16:45:19 +0100

    Refactor jukebox configuration: enhance user setup, firewall rules, and systemd services

--- hosts/jukebox/modules/jukebox.nix
+++ hosts/jukebox/modules/jukebox.nix
@@ -1,38 +1,111 @@
-{ pkgs, ... }:
+{ config, pkgs, lib, ... }:
+
 {
-    # add shairport-sync user
-    users.users.shairport = {
-      description = "Shairport user";
-      isSystemUser = true;
-      createHome = true;
-      home = "/var/lib/shairport-sync";
-      group = "shairport";
-      extraGroups = [ "pulse-access" ];
+  # user and group
+  users = {
+    users.shairport = {
+      description    = "Shairport user";
+      isSystemUser   = true;
+      createHome     = true;
+      home           = "/var/lib/shairport-sync";
+      group          = "shairport";
+      extraGroups    = [ "pulse-access" ];
     };
-    users.groups.shairport = {};
+    groups.shairport = {};
+  };
 
+  # open firewall ports
+  networking.firewall = {
+    interfaces."wlp0s20f3" = {
+      allowedTCPPorts = [
+        3689
+        5353
+        5000
+        7000
+      ];
+      allowedUDPPorts = [
+        5353
+      ];
+      allowedTCPPortRanges = [
+        { from = 32768; to = 60999; }
+      ];
+      allowedUDPPortRanges = [
+        { from = 319; to = 320; }
+        { from = 6000; to = 6009; }
+        { from = 32768; to = 60999; }
+      ];
+    };
+  };
 
-    services.pipewire = {
-        enable = true;
-        alsa.enable = true;
-        pulse.enable = false;
+  # packages
+  environment = {
+    systemPackages = with pkgs; [
+      alsa-utils
+      nqptp
+      shairport-sync-airplay2
+    ];
+  };
+
+  # enable pulseaudio
+  services.pipewire.enable = false;
+  hardware = {
+    pulseaudio = {
+      enable = true;
+      support32Bit = true;
+      systemWide = true;
+    };
+  };
+  
+  # enable Avahi
+  services.avahi = {
+    enable = true;
+    publish.enable = true;
+    publish.userServices = true;
+    allowInterfaces = [ "wlp0s20f3" ];
+  };
+
+  # systemd services
+  systemd.services = {
+    nqptp = {
+      description = "Network Precision Time Protocol for Shairport Sync";
+      wantedBy = [ "multi-user.target" ];
+      after = [ "network.target" ];
+      serviceConfig = {
+        ExecStart = "${pkgs.nqptp}/bin/nqptp";
+        Restart = "always";
+        RestartSec = "5s";
+      };
+    };
+    nix-jukebox = {
+      description = "nix-jukebox shairport-sync instance";
+      wantedBy = [ "multi-user.target" ];
+      after       = [ "network.target" "avahi-daemon.service" ];
+      serviceConfig = {
+        User             = "shairport";
+        Group            = "shairport";
+        ExecStart = "${pkgs.shairport-sync-airplay2}/bin/shairport-sync -c /etc/nix-jukebox.conf";
+        Restart          = "on-failure";
+        RuntimeDirectory = "shairport-sync";
+      };
+    };
+  };
+
+  # write shairport-sync config
+  environment.etc."nix-jukebox.conf".text = ''
+    general =
+    {
+      name = "nix-jukebox";
+      output_backend = "pa";
+      port = 7000;
     };
 
-    services.shairport-sync = {
-        enable = true;
-        openFirewall = true;
-        user = "shairport";
-        group = "shairport";
-        settings = {
-            name = "NixOS-Jukebox";
-            audio = {
-                output_backend = "ao";
-            };
-            ao = {
-                output_device = "default";
-            };
-        };
+    pa =
+    {
+      sink = "alsa_output.pci-0000_00_1f.3-platform-sof_sdw.HDA_Analog";
     };
-
-
+  '';
 }
+
+# run `sudo -u pulse PULSE_RUNTIME_PATH=/run/pulse pactl list sinks short` to display available sinks
+# run `sudo -u pulse alsamixer` to adjust volume levels
+# run `sudo alsactl store` so save the volume levels persistently
