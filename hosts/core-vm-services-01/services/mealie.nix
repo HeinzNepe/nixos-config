10a1c6348176cf60b25945c097d1e193b9234e50
Henrik Nepstad <github@topheinz.com>
2026-05-05 16:50:33 +0200

    Add the base URL to the mealie instance

--- hosts/core-vm-services-01/services/mealie.nix
+++ hosts/core-vm-services-01/services/mealie.nix
@@ -4,5 +4,13 @@
     services.mealie = {
         enable = true;
         port = 9925;
+        settings = {
+            # The publicly accessible URL of your Mealie instance
+            BASE_URL = "https://mealie.topheinz.com";
+
+            # Allow new users to register themselves (set to false to invite-only)
+            ALLOW_SIGNUP = false;
+
+        };
     };
 }
