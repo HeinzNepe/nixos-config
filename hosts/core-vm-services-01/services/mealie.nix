9f723e6ff57c36e50b68a4baa69426a633312a10
Henrik Nepstad <github@topheinz.com>
2026-05-05 16:55:02 +0200

    Remove ALLOW_SIGNUP setting from Mealie configuration

--- hosts/core-vm-services-01/services/mealie.nix
+++ hosts/core-vm-services-01/services/mealie.nix
@@ -7,10 +7,6 @@
         settings = {
             # The publicly accessible URL of your Mealie instance
             BASE_URL = "https://mealie.topheinz.com";
-
-            # Allow new users to register themselves (set to false to invite-only)
-            ALLOW_SIGNUP = false;
-
         };
     };
 }
