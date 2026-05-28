373461f61bdde0efa32cf2cc75c55f4da1eabb46
Henrik Nepstad <github@topheinz.com>
2026-05-28 22:48:51 +0200

    Fix fonts for Geoguessr with the fontconfig option and adding more fonts to fall back to

--- modules/options/fonts.nix
+++ modules/options/fonts.nix
@@ -2,8 +2,19 @@
 
 {
     fonts.enableDefaultPackages = true;
+    fonts.fontconfig.enable = true;
     fonts.packages = with pkgs; [
         nerd-fonts.jetbrains-mono
         roboto
+
+        # Additional fonts for better compatibility with e.g. GeoGuessr
+        noto-fonts
+        noto-fonts-color-emoji
+        liberation_ttf
+        dejavu_fonts
     ];
+
+    environment.sessionVariables = {
+        FONTCONFIG_PATH = "${pkgs.fontconfig}/etc/fonts";
+    };
 }
