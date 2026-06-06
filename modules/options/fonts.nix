cdc3056cd0038a618a9d81942c6619cd94ba9d72
Henrik Nepstad <github@topheinz.com>
2026-06-06 23:04:35 +0200

    Refactor fonts configuration for improved structure and readability

--- modules/options/fonts.nix
+++ modules/options/fonts.nix
@@ -1,20 +1,28 @@
 { config, lib, pkgs, ... }:
 
 {
-    fonts.enableDefaultPackages = true;
-    fonts.fontconfig.enable = true;
-    fonts.packages = with pkgs; [
+  fonts = {
+    enableDefaultPackages = true;
+    fontconfig.enable = true;
+
+    packages = with pkgs; [
         nerd-fonts.jetbrains-mono
         roboto
 
-        # Additional fonts for better compatibility with e.g. GeoGuessr
+        # Base Noto (includes Khmer, Thai, etc.)
         noto-fonts
         noto-fonts-color-emoji
-        liberation_ttf
-        dejavu_fonts
-    ];
 
-    environment.sessionVariables = {
-        FONTCONFIG_PATH = "${pkgs.fontconfig}/etc/fonts";
-    };
+        # CJK support
+        noto-fonts-cjk-sans
+        noto-fonts-cjk-serif
+
+        dejavu_fonts
+        liberation_ttf
+    ];
+  };
+
+  environment.sessionVariables = {
+    FONTCONFIG_PATH = "${pkgs.fontconfig}/etc/fonts";
+  };
 }
