bde142ad160deefcf9d8834d8ca8ae87fb6a4a76
Henrik Xavier Nepstad <github@topheinz.com>
2026-06-12 12:04:09 +0200

    Trying new DISKO layout from the disko examples list

--- hosts/no-vps-01/disko.nix
+++ hosts/no-vps-01/disko.nix
@@ -1,28 +1,53 @@
+# Example to create a bios compatible gpt partition
+# https://github.com/nix-community/nixos-anywhere-examples/blob/main/disk-config.nix
+{ lib, ... }:
 {
   disko.devices = {
-    disk.sda = {
+    disk.disk1 = {
+      device = lib.mkDefault "/dev/sda";
       type = "disk";
-      device = "/dev/sda";
       content = {
         type = "gpt";
         partitions = {
-          ESP = {
-            size = "1G";
+          boot = {
+            name = "boot";
+            size = "1M";
+            type = "EF02";
+          };
+          esp = {
+            name = "ESP";
+            size = "500M";
             type = "EF00";
             content = {
               type = "filesystem";
               format = "vfat";
               mountpoint = "/boot";
-              mountOptions = [ "umask=0077" ];
             };
           };
-
           root = {
+            name = "root";
             size = "100%";
+            content = {
+              type = "lvm_pv";
+              vg = "pool";
+            };
+          };
+        };
+      };
+    };
+    lvm_vg = {
+      pool = {
+        type = "lvm_vg";
+        lvs = {
+          root = {
+            size = "100%FREE";
             content = {
               type = "filesystem";
               format = "ext4";
               mountpoint = "/";
+              mountOptions = [
+                "defaults"
+              ];
             };
           };
         };
