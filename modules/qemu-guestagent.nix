5f5dde49488ede784528a5c14169cd3ec889c4cc
Henrik Xavier Nepstad <github@topheinz.com>
2026-08-06 10:27:00 +0200

    Comment out virtualisation.qemu.guestAgent.enable and clarify guest agent configuration for NixOS

--- modules/qemu-guestagent.nix
+++ modules/qemu-guestagent.nix
@@ -1,5 +1,10 @@
 { config, pkgs, ... }:
 
 {
-    virtualisation.qemu.guestAgent.enable = true;
+    # This is for enabling the guest agent on virtual machines hosted on nixos, not enabling it on the host.
+    #virtualisation.qemu.guestAgent.enable = true;
+
+    # This enables the guest agent on the nix host
+    services.qemuGuest.enable = true;
+
 }
