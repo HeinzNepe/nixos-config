9ac0c564cfb6fea1a099921c0093e5d21f904b07
Henrik Nepstad <github@topheinz.com>
2026-08-16 01:18:25 +0200

    Add core-tv configuration and desktop autologin module

--- flake.nix
+++ flake.nix
@@ -117,6 +117,8 @@
         core-vm-nixhelper-01 = mkNixOSConfig "x86_64-linux" ./hosts/core-vm-nixhelper-01/configuration.nix [];
         core-vm-services-01 = mkNixOSConfig "x86_64-linux" ./hosts/core-vm-services-01/configuration.nix [];
         core-vm-internal-01 = mkNixOSConfig "x86_64-linux" ./hosts/core-vm-internal-01/configuration.nix [];
+        
+        core-tv = mkNixOSConfig "x86_64-linux" ./hosts/core-tv/configuration.nix [];
 
 
         # Jukebox laptop configuration
---
+++ modules/desktop-autologin.nix
@@ -0,0 +1,56 @@
+{ inputs, pkgs, system, ... }:
+
+{
+
+	imports =
+	[ 
+		# Enable greetd display manager
+		#./options/greetd.nix
+
+		# Enable sddm display manager
+		#./options/sddm.nix
+	];
+
+	# Enable the KDE Plasma Desktop Environment.
+	# Enable Plasma 6
+	services.desktopManager.plasma6.enable = true;
+	#services.displayManager.plasma-login-manager.enable = true;
+
+	# Enable sddm display manager
+	services.displayManager.sddm.enable = true;
+
+	# Enable auto-login for the user 'henrik' in the display manager
+	services.displayManager = {
+		autoLogin.enable = true;
+		autoLogin.user = "henrik";
+	};
+
+
+
+	# Enable gdm display manager
+	#services.displayManager.gdm.enable = true; 
+	# Might break. Follow up on https://nixpk.gs/pr-tracker.html?pr=525968
+
+
+
+	# Enable CUPS to print documents.
+	services.printing.enable = true;
+
+	# Enable sound with pipewire.
+	services.pulseaudio.enable = false;
+	security.rtkit.enable = true;
+	services.pipewire = {
+		enable = true;
+		alsa.enable = true;
+		alsa.support32Bit = true;
+		pulse.enable = true;
+		# If you want to use JACK applications, uncomment this
+		#jack.enable = true;
+
+		# use the example session manager (no others are packaged yet so this is enabled by default,
+		# no need to redefine it in your config for now)
+		#media-session.enable = true;
+	};
+
+
+}
