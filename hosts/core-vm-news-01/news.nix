d0df00dc398bb0aaa9f064352f759f6512767b90
Henrik Xavier Nepstad <github@topheinz.com>
2026-08-28 23:35:53 +0200

    WIP

--- hosts/core-vm-news-01/news.nix
+++ hosts/core-vm-news-01/news.nix
@@ -350,6 +350,6 @@ in
     time.timeZone = "Europe/Oslo";
     
     # --- Journal Persistence ---
-    services.journald.settings.Storage = "persistent";
+    services.journald.storage = "persistent";
   };
 }
