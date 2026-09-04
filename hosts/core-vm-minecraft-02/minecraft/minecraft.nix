1d975fef3c442151539d0f0fffa77106a709aea5
Henrik Xavier Nepstad <github@topheinz.com>
2026-09-04 22:01:51 +0200

    Fix: Refactor Minecraft server configuration for improved clarity and consistency in user home and ExecStop commands

--- hosts/core-vm-minecraft-02/minecraft/minecraft.nix
+++ hosts/core-vm-minecraft-02/minecraft/minecraft.nix
@@ -11,18 +11,17 @@
 {
   environment.systemPackages = [ pkgs.jdk21 pkgs.screen ];
 
-  # Set minecraft user home to a writable directory
   users.groups.minecraft = {};
   users.users.minecraft = {
     isSystemUser = true;
     group = "minecraft";
-    home = "/minecraft";  # Fixed: set home to writable directory
+    home = "/minecraft";  # Critical: writable home for .screen
   };
 
   systemd.tmpfiles.rules = [
     "d /minecraft 0755 minecraft minecraft - -"
     "d /minecraft/atm10-sky 0755 minecraft minecraft - -"
-    "d /minecraft/.screen 0700 minecraft minecraft - -"  # Fixed: screen socket dir in home
+    "d /minecraft/.screen 0700 minecraft minecraft - -"  # screen socket directory
   ];
 
   systemd.services.minecraft-atm10-sky = {
@@ -39,17 +38,9 @@
       Restart = "always";
       RestartSec = "10";
 
-      # Match your working example
       ExecStart = "${pkgs.screen}/bin/screen -DmS mc-atm10-sky /bin/bash /minecraft/atm10-sky/run.sh";
 
-      # Multiple ExecStop directives via a script
-      ExecStop = "${pkgs.writeShellScriptBin \"stop-minecraft\" ''
-        #!${pkgs.bash}/bin/bash
-        ${pkgs.screen}/bin/screen -p 0 -S mc-atm10-sky -X eval 'stuff \"say SERVER SHUTTING DOWN IN 5 SECONDS. SAVING ALL MAPS...\"\\015'
-        ${pkgs.coreutils}/bin/sleep 5
-        ${pkgs.screen}/bin/screen -p 0 -S mc-atm10-sky -X eval 'stuff \"save-all\"\\015'
-        ${pkgs.screen}/bin/screen -p 0 -S mc-atm10-sky -X eval 'stuff \"stop\"\\015'
-      ''}";
+      ExecStop = "${pkgs.screen}/bin/screen -p 0 -S mc-atm10-sky -X quit";
 
       Environment = [
         "JAVA_HOME=${pkgs.jdk21}/lib/openjdk"
