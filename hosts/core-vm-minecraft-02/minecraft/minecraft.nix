c68349c2d0bd399a77134fa6d1be1c82c6028c32
Henrik Xavier Nepstad <github@topheinz.com>
2026-09-04 21:59:52 +0200

    Fix: Update Minecraft server configuration for improved directory structure and service management

--- hosts/core-vm-minecraft-02/minecraft/minecraft.nix
+++ hosts/core-vm-minecraft-02/minecraft/minecraft.nix
@@ -11,46 +11,50 @@
 {
   environment.systemPackages = [ pkgs.jdk21 pkgs.screen ];
 
+  # Set minecraft user home to a writable directory
   users.groups.minecraft = {};
   users.users.minecraft = {
     isSystemUser = true;
     group = "minecraft";
+    home = "/minecraft";  # Fixed: set home to writable directory
   };
 
   systemd.tmpfiles.rules = [
-    "d /run/minecraft 0700 minecraft minecraft - -"
+    "d /minecraft 0755 minecraft minecraft - -"
+    "d /minecraft/atm10-sky 0755 minecraft minecraft - -"
+    "d /minecraft/.screen 0700 minecraft minecraft - -"  # Fixed: screen socket dir in home
   ];
 
   systemd.services.minecraft-atm10-sky = {
     description = "Minecraft Server: ATM10 Sky";
 
-    after = [ "network.target" "systemd-tmpfiles-setup.service" ];
+    after = [ "network.target" ];
     wantedBy = [ "multi-user.target" ];
 
     serviceConfig = {
-      Type = "forking";
       User = "minecraft";
       Group = "minecraft";
       WorkingDirectory = "/minecraft/atm10-sky";
 
-      Restart = "on-failure";
-      RestartSec = "30";
+      Restart = "always";
+      RestartSec = "10";
 
-      ExecStart = "${pkgs.writeShellScriptBin \"start-minecraft\" ''
+      # Match your working example
+      ExecStart = "${pkgs.screen}/bin/screen -DmS mc-atm10-sky /bin/bash /minecraft/atm10-sky/run.sh";
+
+      # Multiple ExecStop directives via a script
+      ExecStop = "${pkgs.writeShellScriptBin \"stop-minecraft\" ''
         #!${pkgs.bash}/bin/bash
-        export SCREENDIR=/run/minecraft
-        mkdir -p "$SCREENDIR"
-        chmod 700 "$SCREENDIR"
-        exec ${pkgs.screen}/bin/screen -dmS mc-atm10-sky /bin/bash /minecraft/atm10-sky/run.sh
+        ${pkgs.screen}/bin/screen -p 0 -S mc-atm10-sky -X eval 'stuff \"say SERVER SHUTTING DOWN IN 5 SECONDS. SAVING ALL MAPS...\"\\015'
+        ${pkgs.coreutils}/bin/sleep 5
+        ${pkgs.screen}/bin/screen -p 0 -S mc-atm10-sky -X eval 'stuff \"save-all\"\\015'
+        ${pkgs.screen}/bin/screen -p 0 -S mc-atm10-sky -X eval 'stuff \"stop\"\\015'
       ''}";
-
-      ExecStop = "${pkgs.screen}/bin/screen -p 0 -S mc-atm10-sky -X quit";
 
       Environment = [
         "JAVA_HOME=${pkgs.jdk21}/lib/openjdk"
         "PATH=${pkgs.jdk21}/bin:${pkgs.screen}/bin:${pkgs.coreutils}/bin:${pkgs.procps}/bin:${pkgs.bash}/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
         "TERM=xterm-256color"
-        "SCREENDIR=/run/minecraft"
       ];
     };
   };
